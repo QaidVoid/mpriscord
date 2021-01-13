deb3c9d7a77153f02ab43c471eefd4996a25332d
QaidVoid <12017109+QaidVoid@users.noreply.github.com>
2021-01-13 11:38:30 +0545

    Basic method_call setup returning reply

--- include/dbus.h
+++ include/dbus.h
@@ -1,20 +1,23 @@
 #include <dbus/dbus.h>
 #include <iostream>
+#include <vector>
+
+class Connection;
+
+template <typename T>
+class Proxy
+{
+public:
+    T method_call(std::string interface, std::string method);
+    Connection *connection;
+    std::string bus_name;
+    std::string path;
+};
 
 class Connection
 {
 public:
     Connection();
-    Proxy *WithProxy(std::string bus_name, std::string path);
-
-private:
+    Proxy<Connection *> *WithProxy(std::string bus_name, std::string path);
     DBusConnection *connection;
-};
-
-class Proxy
-{
-public:
-    Connection *connection;
-    std::string bus_name;
-    std::string path;
 };
--- src/dbus.cpp
+++ src/dbus.cpp
@@ -18,11 +18,32 @@ Connection::Connection()
     connection = conn;
 }
 
-Proxy *Connection::WithProxy(std::string bus_name, std::string path)
+Proxy<Connection*> *Connection::WithProxy(std::string bus_name, std::string path)
 {
-    Proxy *proxy = new Proxy;
+    auto *proxy = new Proxy<Connection*>;
     proxy->connection = this;
     proxy->bus_name = bus_name;
     proxy->path = path;
     return proxy;
+}
+
+template<typename T>
+T Proxy<T>::method_call(std::string interface, std::string method)
+{
+    DBusError error;
+    DBusMessage *msg, *reply;
+    DBusMessageIter iter, sub;
+    std::vector<std::string> result;
+    char *res;
+
+    dbus_error_init(&error);
+    msg = dbus_message_new_method_call(bus_name.c_str(), path.c_str(), interface.c_str(), method.c_str());
+    reply = dbus_connection_send_with_reply_and_block(
+        connection->connection,
+        msg,
+        1000,
+        &error);
+    abort_on_error(&error);
+    dbus_message_unref(msg);
+    return reply;
 }
